4d1a84e5a4825e337b649e1fd85bda3f6573005e
wezer-pixel <76890104+wezer-pixel@users.noreply.github.com>
2023-02-04 19:43:59 +0300

    initial commit

---
+++ app/Http/Controllers/GitHubController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Auth;
+use Exception;
+use Laravel\Socialite\Facades\Socialite;
+class GitHubController extends Controller
+{
+    public function gitRedirect()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+       
+    public function gitCallback()
+    {
+
+       try {
+     
+            $user = Socialite::driver('github')->user();
+      
+            $searchUser = User::where('github_id', $user->id)->first();
+      
+            if($searchUser){
+      
+                Auth::login($searchUser);
+     
+                return redirect('/dashboard');
+      
+            }else{
+                $gitUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'github_id'=> $user->id,
+                    'auth_type'=> 'github',
+                    'password' => encrypt('gitpwd059')
+                ]);
+     
+                Auth::login($gitUser);
+      
+                return redirect('/dashboard');
+            }
+     
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        } 
+    } 
+}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\GitHubController;
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('auth/github', [GitHubController::class, 'gitRedirect']);
+
+Route::get('auth/github/callback', [GitHubController::class, 'gitCallback']);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
